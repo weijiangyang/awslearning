63267df81fc97df34726b5699bf6a6623602dfcb
weijiangyang <weijiangyang@laposte.net>
2022-11-07 09:25:17 +0100

    first commit

---
+++ src/Controller/Admin/FichierCrudController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Fichier;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class FichierCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Fichier::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+           
+            TextField::new('nom'),
+            TextField::new('imageFile', 'Upload')
+            ->setFormType(VichImageType::class)
+            ->onlyOnForms(),
+            ImageField::new('imageName', 'Ficher')
+            ->setBasePath('http://127.0.0.1:9000/awslearning')
+            ->hideOnForm()
+        ];
+    }
+    
+}
---
+++ src/Entity/Fichier.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FichierRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\HttpFoundation\File\File;
+
+#[ORM\Entity(repositoryClass: FichierRepository::class)]
+#[Vich\Uploadable]
+class Fichier
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[Vich\UploadableField(mapping: 'fichier', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(type: 'string', nullable: true)]
+    private ?string $imageName = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+}
